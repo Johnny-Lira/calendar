b65f5bd39a4d95338badb024eb316344dc4ab1f5
Johnny Lira <johnnyjlira@gmail.com>
2019-12-12 19:48:22 -0300

    Firt commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ]
+  });
+  
+  calendar.render();
+});
